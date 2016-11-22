71c358fe7ab46360c45da3e5f67e857ee47673a7
Philipp Zettl <zettl.philipp@gmail.com>
2016-11-22 01:16:24 +0100

    initialised git

---
+++ elements/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from tinymce.models import HTMLField
+from django.utils import timezone
+# Create your models here.
+
+
+class Tag(models.Model):
+	tag = models.CharField(blank=True, max_length=255)
+
+	def __str__(self):
+		return self.tag
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+	tags = Tag
+
+	def publish(self):
+		self.published_date=timezone.now()
+		self.save()
+	
+	def __str__(self):
+		return self.title
---
+++ elements/views.py
@@ -0,0 +1,59 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, login
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+# Create your views here.
+
+def home(request):
+	return render(request, 'wiki/home.html', {})
+
+def post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'elements/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'elements/post_detail.html', {'post':post})
+
+
+@login_required(login_url='/login/')
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'elements/post_edit.html', {'form': form})
+
+
+@login_required(login_url='/login/')
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'elements/post_edit.html', {'form': form})
+
+
+def logout_view(request):
+	logout(request)
+	return render(request, 'wiki/home.html', {})
+
+def login_view(request):
+	form = AuthenticationForm(instance=post)
+	return render(request, 'registration/login.html', {'form': form})
