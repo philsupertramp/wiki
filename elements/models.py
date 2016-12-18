564e324a802eb702eb1cf4d65a201d576415becc
Philipp Zettl <zettl.philipp@gmail.com>
2016-12-18 22:53:28 +0100

    changed forms.py settings for models

--- elements/models.py
+++ elements/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.utils import timezone
+
 # Create your models here.
 
 
 class Tag(models.Model):
-    tag = models.CharField(blank=True, max_length=255)
+    name = models.CharField(max_length=50)
+    group = models.CharField(max_length=100)
+    
+    class Meta:
+        ordering=['name',]
+    
+    
+    
+
+    def delete(self,*args, **kwargs):
+        super(Tag, self).delete(*args, **kwargs)
 
     def __str__(self):
-        return self.tag
+        return self.name
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -15,7 +26,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ForeignKey(Tag)
 
     def publish(self):
         self.published_date=timezone.now()
--- wiki/settings.py
+++ wiki/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'elements',
-    'django_summernote',
+    'tags',
 )
 
 MIDDLEWARE_CLASSES = (
