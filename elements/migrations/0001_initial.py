39f1637bf1a128a553f6d52121fdd0947d0cd421
Philipp Zettl <zettl.philipp@gmail.com>
2017-07-10 17:08:43 +0200

    typos

--- elements/migrations/0001_initial.py
+++ elements/migrations/0001_initial.py
@@ -2,16 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import ckeditor.fields
 import django.utils.timezone
 from django.conf import settings
+import ckeditor.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('tags', '0001_initial'),
     ]
 
     operations = [
@@ -23,8 +22,8 @@ class Migration(migrations.Migration):
                 ('text', ckeditor.fields.RichTextField()),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
                 ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('tags', models.CharField(max_length=55)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
-                ('tags', models.ForeignKey(null=True, to='tags.Tag')),
             ],
         ),
     ]
