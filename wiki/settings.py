ea18bb83ccbf2e3af172736c3b37b87c6ee060b2
Philipp Zettl <zettl.philipp@gmail.com>
2017-07-11 17:20:10 +0200

    setting changes

--- wiki/settings.py
+++ wiki/settings.py
@@ -28,6 +28,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
 	'.time-dev.de',
+	'localhost',
+    '127.0.0.1',
+    '[::1]',
 	]
 
 
@@ -63,7 +66,7 @@ ROOT_URLCONF = 'wiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['static/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
