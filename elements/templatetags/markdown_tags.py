a237b3afde3ff509ec39ad308f04f797de878685
Philipp Zettl <zettl.philipp@gmail.com>
2021-04-24 21:40:01 +0200

    add meta tags

--- elements/templatetags/markdown_tags.py
+++ elements/templatetags/markdown_tags.py
@@ -1,3 +1,5 @@
+import re
+
 import markdown
 from django import template
 from difflib import unified_diff, Differ, HtmlDiff, ndiff
@@ -28,3 +30,18 @@ def format_diff(value):
     backslash = "\n"
     values = [f'<span style="color: {"red" if i.startswith("-") else "green" if (i.startswith("+") or i.startswith("?")) else "black"}">{i[:-1] if i.endswith(backslash) else i}</span><br/>' for i in value.split(backslash)]
     return ''.join(values)
+
+
+@register.filter
+def strip_text(value):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', value)
+    cleantext = cleantext.replace('#', '')
+    cleantext = cleantext.replace('*', '')
+    cleantext = cleantext.replace('_', '')
+    return cleantext
+
+
+@register.filter
+def shorten(value, length):
+    return value[:length]
