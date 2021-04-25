381b63abce8e59cbf13200a8870756434394916c
Philipp Zettl <zettl.philipp@gmail.com>
2021-04-25 11:33:45 +0200

    more meta tags

--- elements/templatetags/markdown_tags.py
+++ elements/templatetags/markdown_tags.py
@@ -34,11 +34,32 @@ def format_diff(value):
 
 @register.filter
 def strip_text(value):
+    # (REMOVE <SCRIPT> to </script> and variations)
+    pattern = r'<[ ]*script.*?\/[ ]*script[ ]*>'  # mach any char zero or more times
+    text = re.sub(pattern, '', value, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+    # (REMOVE HTML <STYLE> to </style> and variations)
+    pattern = r'<[ ]*style.*?\/[ ]*style[ ]*>'  # mach any char zero or more times
+    text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+    # (REMOVE HTML <META> to </meta> and variations)
+    pattern = r'<[ ]*meta.*?>'  # mach any char zero or more times
+    text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+    # (REMOVE HTML COMMENTS <!-- to --> and variations)
+    pattern = r'<[ ]*!--.*?--[ ]*>'  # mach any char zero or more times
+    text = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
+
+    # (REMOVE HTML DOCTYPE <!DOCTYPE html to > and variations)
+    pattern = r'<[ ]*\![ ]*DOCTYPE.*?>'  # mach any char zero or more times
+    value = re.sub(pattern, '', text, flags=(re.IGNORECASE | re.MULTILINE | re.DOTALL))
     cleanr = re.compile('<.*?>')
     cleantext = re.sub(cleanr, '', value)
     cleantext = cleantext.replace('#', '')
     cleantext = cleantext.replace('*', '')
     cleantext = cleantext.replace('_', '')
+    cleantext = cleantext.replace('`', '')
+    cleantext = cleantext.replace('\n', ' ')
     return cleantext
 
 
