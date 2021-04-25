38e921aea6beecbda6276800fee7cd89ee92b06f
Philipp Zettl <zettl.philipp@gmail.com>
2021-04-25 19:56:37 +0200

    add sitemap

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import re_path, path, include
 from elements import views as elements_views
+from wiki.sitemaps import StaticSitemap, PageSitemap
+from django.contrib.sitemaps.views import sitemap
+
+sitemaps = {
+    'static': StaticSitemap,
+    'pages': PageSitemap,
+}
 
 urlpatterns = [
     # Examples:
@@ -11,6 +18,7 @@ urlpatterns = [
     re_path(r'', include('elements.urls')),
     re_path(r'', include('pages.urls')),
     re_path(r'^markdownx/', include('markdownx.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 
     # accounting urls
     path('accounts/', include('django.contrib.auth.urls')),
