23e2385dfb33d9b4b040543c5fb46dba8ead0f03
Philipp Zettl <zettl.philipp@gmail.com>
2021-04-25 19:57:42 +0200

    .. well and migrations

---
+++ wiki/sitemaps.py
@@ -0,0 +1,29 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+from pages.models import Page
+
+
+class StaticSitemap(Sitemap):
+    protocol = 'https'
+
+    def items(self):
+        return ['page_list', ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+class PageSitemap(Sitemap):
+    protocol = 'https'
+    changefreq = 'weekly'
+    priority = 0.8
+
+    def items(self):
+        return Page.objects.all().order_by('-edited_at')
+
+    def lastmod(self, obj):
+        return obj.edited_at
+
+    def location(self, item):
+        return reverse('page_detail', kwargs={'slug': item.slug})
